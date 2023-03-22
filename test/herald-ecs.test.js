6d9cd81d667db4f0622eb6c99e3c9194688d9737
Wayne Olson <62842516+wayneoco@users.noreply.github.com>
2023-03-22 05:38:01 -0700

    dev: initial commit

---
+++ test/herald-ecs.test.js
@@ -0,0 +1,17 @@
+// const cdk = require('aws-cdk-lib');
+// const { Template } = require('aws-cdk-lib/assertions');
+// const HeraldEcs = require('../lib/herald-ecs-stack');
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/herald-ecs-stack.js
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//   // WHEN
+//   const stack = new HeraldEcs.HeraldEcsStack(app, 'MyTestStack');
+//   // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
